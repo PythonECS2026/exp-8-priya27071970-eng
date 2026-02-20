964f0de96e8e18cdf9eb5b478e8b31596ac83f0f
priya27071970-eng <priya27071970@gmail.com>
2026-02-20 20:27:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 # the transition from C to Python syntax.
 # Coder:
 # Date:
-
-print("--- Pattern Printer ---\n")
-
-
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end="")
+    print()
 # Write your code here
+
